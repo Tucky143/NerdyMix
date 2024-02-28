1f632cfb662f497ef429519e9c44f33049154254
PooPmeep <112079900+NerdyPuzzle@users.noreply.github.com>
2024-02-28 22:25:56 +0200

    Initial commit

---
+++ src/main/java/net/nerdypuzzle/forgemixins/Launcher.java
@@ -0,0 +1,22 @@
+package net.nerdypuzzle.forgemixins;
+
+import net.mcreator.plugin.JavaPlugin;
+import net.mcreator.plugin.Plugin;
+import net.mcreator.plugin.events.PreGeneratorsLoadingEvent;
+import net.nerdypuzzle.forgemixins.parts.PluginElementTypes;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Launcher extends JavaPlugin {
+
+	private static final Logger LOG = LogManager.getLogger("Loot modifier");
+
+	public Launcher(Plugin plugin) {
+		super(plugin);
+
+		addListener(PreGeneratorsLoadingEvent.class, e -> PluginElementTypes.load());
+
+		LOG.info("Loot modifier plugin was loaded");
+	}
+
+}
