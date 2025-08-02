71bf8b62eae08486d309bfdede27ed276403b11b
Tucky143 <110123789+Tucky143@users.noreply.github.com>
2025-08-02 01:53:18 -0600

    JER (Most Support; xx1.21.4xx)

--- src/main/java/net/tucky143/nerdy/parts/PluginElementTypes.java
+++ src/main/java/net/tucky143/nerdy/parts/PluginElementTypes.java
@@ -1,18 +1,34 @@
 package net.tucky143.nerdy.parts;
 
 import net.mcreator.element.ModElementType;
-import net.tucky143.nerdy.elements.EndBiome;
-import net.tucky143.nerdy.elements.EndBiomeGUI;
-import net.tucky143.nerdy.elements.Endstone;
-import net.tucky143.nerdy.elements.EndstoneGUI;
+import net.tucky143.nerdy.elements.*;
 
 import static net.mcreator.element.ModElementTypeLoader.register;
 
 public class PluginElementTypes {
     public static ModElementType<?> ENDBIOME;
     public static ModElementType<?> ENDSTONE;
+    public static ModElementType<?> JEIRECIPETYPE;
+    public static ModElementType<?> JEIRECIPE;
+    public static ModElementType<?> ANVILRECIPE;
+    public static ModElementType<?> JEIINFORMATION;
 
     public static void load() {
+        JEIRECIPETYPE = register(
+                new ModElementType<>("jeirecipetype", (Character) 'C', JeiRecipeTypeGUI::new, JeiRecipeType.class)
+        );
+
+        JEIRECIPE = register(
+                new ModElementType<>("jeirecipe", (Character) 'R', JeiRecipeGUI::new, JeiRecipe.class)
+        );
+
+        ANVILRECIPE = register(
+                new ModElementType<>("anvilrecipe", (Character) 'A', AnvilRecipeGUI::new, AnvilRecipe.class)
+        );
+
+        JEIINFORMATION = register(
+                new ModElementType<>("jeiinformation", (Character) 'I', JeiInformationGUI::new, JeiInformation.class)
+        );
 
         ENDBIOME = register(
                 new ModElementType<>("endbiome", (Character) 'E', EndBiomeGUI::new, EndBiome.class)
